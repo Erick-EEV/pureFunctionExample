4b2b7a747ac3f6aa222b547103ceeee66a67dca9
Erick-EEV <EEVERICKEEV@gmail.com>
2021-04-11 13:41:51 -0400

    pureFunction created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Example from './container/Example'
 
 function App() {
   return (
     <div className="App">
+    <Example/>
     </div>
   );
 }
---
+++ src/container/Example.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Example = () => {
+
+    pureFunction = (input) => {
+        let outcome = input * 2
+        console.log(outcome)
+    }
+
+    return (
+        <div>
+            <h2>Hello World</h2>
+        </div>
+    );
+}
+
+export default Example;
+
